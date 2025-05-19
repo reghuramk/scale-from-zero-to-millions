cae61a4255451eaecf06be1ec970b5e1f8440d7a
reghuramk <reghuramk04@gmail.com>
2025-05-19 23:04:43 +0100

    added url shorterner

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/users';
+import shortenerRoutes from './routes/shortener'; 
 
 const app = express();
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/users', userRoutes);           
+app.use('/', shortenerRoutes);     
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
