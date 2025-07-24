879c596e19869948a1444df91460f8c1ccd6ac15
reghuramk <reghuramk04@gmail.com>
2025-07-24 21:52:49 +0100

    added email verification flow

---
+++ src/utils/sendverificationemali.ts
@@ -0,0 +1,11 @@
+// import sgMail from "@sendgrid/mail";
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const msg = {
+//   to: user.email,
+//   from: "noreply@yourdomain.com",
+//   subject: "Verify your email",
+//   html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`,
+// };
+
+// await sgMail.send(msg);
